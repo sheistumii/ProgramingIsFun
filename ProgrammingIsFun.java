f8783f85b86685cfdb9690290baee6903d2af738
sheistumii <84107452+sheistumii@users.noreply.github.com>
2021-05-14 16:20:44 +0200

    Add files via upload

---
+++ ProgrammingIsFun.java
@@ -0,0 +1,14 @@
+/**********************************************************************
+* HelloGUI.java
+*sheistumii
+*
+***********************************************************************/
+
+import javax.swing.JOptionPane;
+public class ProgrammingIsFun
+{
+	public static void main(string[] args)
+	{
+		JOptionPane.showMessageDialog(null, "Programming is Fun!");
+	}
+}
